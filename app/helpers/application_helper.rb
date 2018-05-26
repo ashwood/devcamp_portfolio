490e1a10c4fd147072200bb8a944daa6ccd8143e
Rosalynn <admin@rosalynnashwood.com>
2018-05-26 01:08:06 -0700

    Implement BEM naming methodology and refactor all exisiting styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,16 @@
 module ApplicationHelper
   def login_helper
     if current_user.is_a?(GuestUser)
-      (link_to "Login", new_user_session_path) +
-      (link_to "Register", new_user_registration_path)
+      (link_to "Login", new_user_session_path,
+          class: 'sidebar__link
+                  header__link') +
+      (link_to "Register", new_user_registration_path,
+          class: 'sidebar__link
+                  header__link')
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      (link_to "Logout", destroy_user_session_path, method: :delete,
+          class: 'sidebar__link
+                  header__link')
     end
   end
 
@@ -21,6 +27,6 @@ module ApplicationHelper
   end
 
   def active_class(link_path)
-    current_page?(link_path) ? "active" : ""
+    current_page?(link_path) ? "--active" : ""
   end
 end
