6fefdeb371dd419240bfd27e34a0308ad0ece245
Rosalynn <admin@rosalynnashwood.com>
2018-06-04 21:52:52 -0700

    Integrate markdown gem with helper methods

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -7,4 +7,24 @@ module BlogsHelper
   def truncate_helper text, path, style = ''
     truncate(text, length: 275) { link_to "(continued)", path, class: "#{style}" }
   end
+
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language).div
+    end
+  end
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      auto_links: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
 end
