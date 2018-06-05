50f189ce522003a82059b81a3bc2a7bc67c15352
Rosalynn <admin@rosalynnashwood.com>
2018-06-04 20:24:15 -0700

    Build helper method to truncate blog body in blog index

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -3,4 +3,8 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 30
   end
+
+  def truncate_helper text, path, style = ''
+    truncate(text, length: 275) { link_to "(continued)", path, class: "#{style}" }
+  end
 end
