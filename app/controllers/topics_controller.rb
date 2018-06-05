9d687c421e9e865333eaedf3aef6d6f26ae6f569
Rosalynn <admin@rosalynnashwood.com>
2018-06-04 19:13:16 -0700

    Implement functionality to topic widget in blog sidebar

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'blog'
 
   def show
@@ -10,4 +11,10 @@ class TopicsController < ApplicationController
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(5)
     end
   end
+
+  private
+    def set_sidebar_topics
+      @sidebar_topics = Topic.with_blogs
+    end
 end
+
